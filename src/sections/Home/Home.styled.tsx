c7b2830c988de9b1a1f8d3e947d76674932ff181
Ignacio Vallejos <nacho.vallejos17@outlook.com>
2021-03-20 21:03:53 -0300

    Fix display in Home component

--- src/sections/Home/Home.styled.tsx
+++ src/sections/Home/Home.styled.tsx
@@ -5,20 +5,14 @@ export const HomeWrapper = styled.section`
   min-height: calc(100vh - 96px);
 `;
 
-export const HomeContainer = styled(Container)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
+export const HomeContainer = styled(Container)``;
 
 export const HomeLogo = styled.div(
   ({ theme }) => css`
     width: 150px;
     height: 150px;
 
-    margin-top: 6rem;
-    margin-bottom: 1.5rem;
+    margin: 6rem auto 1.5rem;
 
     border: 2px solid ${theme.colors.primary};
     border-radius: 50%;
@@ -30,6 +24,7 @@ export const HomeTitle = styled.h2(
   ({ theme }) => css`
     font-size: 2.25rem;
     font-weight: normal;
+    text-align: center;
     text-transform: uppercase;
     color: ${theme.colors.shade};
   `
